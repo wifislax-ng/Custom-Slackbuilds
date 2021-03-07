68cdfb04d5fcfbf6347e00cd8589132a89069f0c
wifislax-ng <wifislaxdevelopers@hotmail.com>
2021-03-07 22:25:18 +0100

    migrando mas cosas a git tarballs

---
+++ mdk4/fetch.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+REPO=https://github.com/aircrack-ng
+PRGNAM=mdk4
+CLONE=$REPO/$PRGNAM
+
+rm -rf $PRGNAM
+git clone $CLONE
+
+( cd $PRGNAM 
+  git checkout master || exit 1
+)
+
+HEADISAT="$( cd $PRGNAM && git log -1 --format=%h )"
+DATE="$( cd $PRGNAM && git log -1 --format=%cd --date=format:%Y%m%d )"
+LONGDATE="$( cd $PRGNAM && git log -1 --format=%cd --date=format:%c )"
+mv $PRGNAM $PRGNAM-${DATE}_${HEADISAT}
+tar cf $PRGNAM-${DATE}_${HEADISAT}.tar $PRGNAM-${DATE}_${HEADISAT}
+xz -9 -f $PRGNAM-${DATE}_${HEADISAT}.tar
+rm -rf $PRGNAM-${DATE}_${HEADISAT}
+touch -d "$LONGDATE" $PRGNAM-${DATE}_${HEADISAT}.tar.xz
